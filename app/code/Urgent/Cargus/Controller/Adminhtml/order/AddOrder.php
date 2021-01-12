b8fa799ace55aff67e8b2da7e1c6fa747e9bcef3
Tudor <pascu.tn@pg.com>
2021-01-12 16:19:03 +0200

    adauga comanda

--- app/code/Urgent/Cargus/Controller/Adminhtml/order/AddOrder.php
+++ app/code/Urgent/Cargus/Controller/Adminhtml/order/AddOrder.php
@@ -4,13 +4,14 @@ namespace Urgent\Cargus\Controller\Adminhtml\order;
 
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
-use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\CsrfAwareActionInterface;
 use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
-use Urgent\Cargus\Model\UrgentCargus;
+use Magento\Store\Model\StoreManagerInterface;
 
 /**
  * Class Index
@@ -23,16 +24,35 @@ class AddOrder extends Action implements CsrfAwareActionInterface
     protected $resultPageFactory;
 
     /**
+     * @var ResourceConnection
+     */
+    private $_resource;
+    /**
+     * @var StoreManagerInterface
+     */
+    private $_storeManager;
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
      * Index constructor.
      *
      * @param Context $context
      * @param PageFactory $resultPageFactory
+     * @param ResourceConnection $resource
+     * @param StoreManagerInterface $storeManager
+     * @param ScopeConfigInterface $scopeConfig
      */
-    public function __construct(Context $context, PageFactory $resultPageFactory)
+    public function __construct(Context $context, PageFactory $resultPageFactory, ResourceConnection $resource, StoreManagerInterface $storeManager, ScopeConfigInterface $scopeConfig)
     {
         parent::__construct($context);
 
         $this->resultPageFactory = $resultPageFactory;
+        $this->_resource = $resource;
+        $this->_storeManager = $storeManager;
+        $this->scopeConfig = $scopeConfig;
     }
 
     public function createCsrfValidationException(RequestInterface $request): ? InvalidRequestException
@@ -46,12 +66,174 @@ class AddOrder extends Action implements CsrfAwareActionInterface
     }
 
     /**
-     * Load the page defined in view/adminhtml/layout/order_index.xml
-     *
      * @return Page
      */
     public function execute()
     {
-        echo 'ok';
+        $orderId = $this->getRequest()->getParam('id');
+        $orderName = $this->getRequest()->getParam('id_long');
+
+        $connection = $this->_resource->getConnection(ResourceConnection::DEFAULT_CONNECTION);
+
+        $existing = $connection->fetchAll("SELECT `id` FROM `awb_expeditii` WHERE `order_id`='".$orderName."'");
+
+        if ($existing){
+            echo 'old';
+            die();
+        }
+
+        $query = "SELECT
+                    so.weight,
+                    so.order_currency_code,
+                    so.created_at,
+                    so.grand_total,
+                    so.shipping_amount,
+                    so.shipping_tax_amount,
+                    so.total_paid,
+                    soa.region,
+                    soa.region_id,
+                    soa.city,
+                    soa.street,
+                    soa.postcode,
+                    soa.telephone,
+                    soa.lastname,
+                    soa.firstname,
+                    soa.middlename,
+                    soa.company,
+                    soa.email,
+                    sop.method,
+                    GROUP_CONCAT(soi.name SEPARATOR '; ') AS continut
+                FROM sales_order so
+                LEFT JOIN sales_order_address soa ON so.shipping_address_id = soa.entity_id
+                LEFT JOIN sales_order_item soi ON so.entity_id = soi.order_id
+                LEFT JOIN sales_order_payment sop ON so.entity_id = sop.parent_id
+                WHERE
+                    so.entity_id = " . $orderId . " AND
+                    soi.parent_item_id IS NULL AND
+                    soa.address_type = 'shipping'
+                GROUP BY so.increment_id,sop.method
+                LIMIT 0,1";
+
+        $data = $connection->fetchAll($query)[0];
+
+        $base2ron = 1;
+        $baseCode = $data['order_currency_code'];
+
+        if ($baseCode != 'RON') {
+            $defaultCurrency = $this->_storeManager->getStore()->getCurrentCurrencyCode();
+            $allowedCurrencies = $this->_storeManager->getStore()->getAllowedCurrencies();
+
+            $rates = $this->_storeManager->getStore()->getBaseCurrency()->getCurrencyRates($defaultCurrency, array_values($allowedCurrencies));
+            $base2ron = $rates['RON'] / $rates[$baseCode];
+        }
+
+        $preferences = unserialize($this->scopeConfig->getValue('urgent/cargus/preferences'));
+
+
+        if ($preferences['payer'] != 1 || ($preferences['fixed_cost_transport'] || $preferences['fixed_cost_transport'] == '0')) {
+            $payer = 1;
+            $valoareRamburs = round($data['grand_total'] * $base2ron, 2);
+        } else {
+            $payer = 2;
+            $valoareRamburs = round(($data['grand_total'] - $data['shipping_amount'] - $data['shipping_tax_amount']) * $base2ron, 2);
+        }
+
+        if ($data['shipping_amount'] == 0) {
+            $payer = 1;
+        }
+
+        if ($preferences['insurance'] == '1') {
+            $DeclaredValue = round(($data['grand_total'] - $data['shipping_amount']) * $base2ron, 2);
+        } else {
+            $DeclaredValue = 0;
+        }
+
+        if (strpos($data['method'], 'cashondelivery') !== false) {
+            if ($preferences['repayment'] == 1) {
+                $CashRepayment = 0;
+                $BankRepayment = round($valoareRamburs, 2);
+            } else {
+                $CashRepayment = round($valoareRamburs, 2);
+                $BankRepayment = 0;
+            }
+        } else {
+            $CashRepayment = 0;
+            $BankRepayment = 0;
+        }
+
+        $query = "SELECT * FROM directory_country_region WHERE `country_id` = 'RO' AND `region_id` = '".$data['region_id']."'";
+        $judetResult = $connection->fetchAll($query);
+
+        $weight = ($data['weight'] < 1 ? 1 : ceil($data['weight']));
+
+        if ($weight > 1) {
+            $no_envelopes = 0;
+            $no_parcels = 1;
+        } else {
+            $no_envelopes = ($preferences['expedition_type'] == 'envelope' ? 1 : 0);
+            $no_parcels = ($preferences['expedition_type'] != 'envelope' ? 1 : 0);
+        }
+
+        $query = "INSERT INTO `awb_expeditii` (
+                            `order_id`,
+                            `pickup_location_id`,
+                            `cod_bara`,
+                            `nume_dest`,
+                            `judet_dest`,
+                            `localitate_dest`,
+                            `adresa_dest`,
+                            `contact_dest`,
+                            `telefon_dest`,
+                            `email_dest`,
+                            `plicuri`,
+                            `colete`,
+                            `kilograme`,
+                            `valoare_declarata`,
+                            `ramburs_numerar`,
+                            `ramburs_cont`,
+                            `platitor_expeditie`,
+                            `livrare_sambata`,
+                            `livrare_dimineata`,
+                            `deschidere_colet`,
+                            `observatii`,
+                            `continut`,
+                            `status`
+                        ) VALUES (
+                            '".$this->clear($orderName)."',
+                            '".$preferences['collect_point']."',
+                            '',
+                            '".($this->clear($data['company']) ? $this->clear($data['company']) : $this->clear($data['firstname']).' '.$this->clear($data['lastname']))."',
+                            '".$this->clear($judetResult[0]['code'])."',
+                            '".$this->clear($data['city'])."',
+                            '".$this->clear($data['street'])."',
+                            '".$this->clear($data['firstname']).' '.$this->clear($data['lastname'])."',
+                            '".$this->clear($data['telephone'])."',
+                            '".$this->clear($data['email'])."',
+                            '".$no_envelopes."',
+                            '".$no_parcels."',
+                            '".$weight."',
+                            '".$DeclaredValue."',
+                            '".$CashRepayment."',
+                            '".$BankRepayment."',
+                            '".$payer."',
+                            '".$preferences['saturday_delivery']."',
+                            '".$preferences['morning_delivery']."',
+                            '".$preferences['open_package']."',
+                            '',
+                            '".$this->clear($data['continut'])."',
+                            '0'
+                        );";
+        $connection->query($query);
+
+        $out = $connection->fetchAll("SELECT * FROM `awb_expeditii` WHERE `order_id` = '".$this->clear($orderName)."' LIMIT 1");
+        if (count($out) > 0) {
+            echo 'ok';
+        } else {
+            echo 'bad';
+        }
+    }
+
+    public function clear($var) {
+        return addslashes(trim($var));
     }
 }
--- app/code/Urgent/Cargus/view/adminhtml/web/js/urgentcargus.js
+++ app/code/Urgent/Cargus/view/adminhtml/web/js/urgentcargus.js
@@ -7,13 +7,27 @@ function addOrder() {
         let soil = $('.page-title').html();
         soil = soil.replace('# ', '');
 
-        $.ajax({
+        let ajaxRequest;
+
+        ajaxRequest = $.ajax({
             async: false,
-            url: '../cargus/order/addorder?id=' + single_order_id + '&id_long=' + soil + '&timestamp=' + new Date().getTime(),
-            success: function (data) {
-                if (data == 'ok') alert('Comanda curenta a fost adaugata cu succes in lista pentru livrare.');
-                else if (data == 'old') alert('Comanda curenta a fost livrata sau este deja in lista pentru livrare.');
-                else alert('Comanda curenta nu a putut fi adaugata in lista pentru livrare.');
+            url: "../../../../../../../cargus/order/addorder",
+            type: 'POST',
+            data: {
+                id: single_order_id,
+                id_long: soil,
+                timestamp: new Date().getTime(),
+                form_key:FORM_KEY
+            },
+        });
+
+        ajaxRequest.done(function (response, textStatus, jqXHR) {
+            if (response == 'ok') {
+                alert('Comanda curenta a fost adaugata cu succes in lista pentru livrare.');
+            } else if (response == 'old') {
+                alert('Comanda curenta a fost livrata sau este deja in lista pentru livrare.');
+            } else {
+                alert('Comanda curenta nu a putut fi adaugata in lista pentru livrare.');
             }
         });
     });
@@ -25,27 +39,50 @@ function addOrders() {
         let status_final_1 = '';
         let status_final_2 = '';
         let status_final_3 = '';
+        let ajaxRequest;
 
         $("input:checkbox[id^=idscheck]:checked").each(function () {
             let id = $(this).val();
-            // let nexttd = $(this).closest('td').next('td')
             var id_long = $(this).closest('td').next('td').find('div').text();
 
-            $.ajax({
+
+            ajaxRequest = $.ajax({
                 async: false,
-                url: '../cargus/order/addorder?id=' + id + '&id_long=' + id_long + '&timestamp=' + new Date().getTime(),
-                success: function (data) {
-                    if (data == 'ok') status_final_1 = status_final_1 + " - " + id_long + "\n";
-                    else if (data == 'old') status_final_2 = status_final_2 + " - " + id_long + "\n";
-                    else status_final_3 = status_final_3 + " - " + id_long + "\n";
+                url: "../../../../../cargus/order/addorder",
+                type: 'POST',
+                data: {
+                    id: id,
+                    id_long: id_long,
+                    timestamp: new Date().getTime(),
+                    form_key:FORM_KEY
+                },
+            });
+
+            ajaxRequest.done(function (response, textStatus, jqXHR) {
+                if (response == 'ok'){
+                    status_final_1 = status_final_1 + " - " + id_long + "\n";
+                } else if (response == 'old') {
+                    status_final_2 = status_final_2 + " - " + id_long + "\n";
+                } else {
+                    status_final_3 = status_final_3 + " - " + id_long + "\n";
                 }
             });
         });
 
-        if (status_final_1) status_final = status_final + "Comenzile de mai jos au fost adaugate cu succes in lista pentru livrare:\n" + status_final_1;
-        if (status_final_2) status_final = status_final + "Comenzile de mai jos au fost livrate sau sunt deja in lista pentru livrare:\n" + status_final_2;
-        if (status_final_3) status_final = status_final + "Comenzile de mai jos nu au putut fi adaugate in lista pentru livrare:\n" + status_final_3;
-        if (status_final) alert(status_final); else alert('Nu ati selectat nicio comanda pentru a fi adaugata in lista de livrari Urgent Cargus!');
+        if (status_final_1) {
+            status_final = status_final + "Comenzile de mai jos au fost adaugate cu succes in lista pentru livrare:\n" + status_final_1;
+        }
+        if (status_final_2) {
+            status_final = status_final + "Comenzile de mai jos au fost livrate sau sunt deja in lista pentru livrare:\n" + status_final_2;
+        }
+        if (status_final_3) {
+            status_final = status_final + "Comenzile de mai jos nu au putut fi adaugate in lista pentru livrare:\n" + status_final_3;
+        }
+        if (status_final) {
+            alert(status_final);
+        } else {
+            alert('Nu ati selectat nicio comanda pentru a fi adaugata in lista de livrari Urgent Cargus!');
+        }
     });
 }
 
